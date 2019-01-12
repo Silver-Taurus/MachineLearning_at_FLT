33fd1c96a5eda03b052436425ff80af37396cc7d
Silver-Taurus <silvertaurus4869@gmail.com>
2019-01-12 15:58:36 +0530

    Python updation

---
+++ Python/01_Intro/source.py
@@ -0,0 +1,31 @@
+# Variable declaration
+var = 20
+a = b = c = 10
+
+# Python Tokens
+#   --> Keywords
+#   --> Identifiers
+#   --> Literals
+#   --> Operators
+
+# Keywords
+#    --> Python Keywords are special reserved words
+#    --> Convey a special meaning to the compiler/interpreter
+#    --> Each keyword have a special meaning and a specific operation
+#    --> Keyword cannot be used as a variable
+# False	 class	   finally	is	      return
+# None	 continue  for	    lambda	  try
+# True	 def	   from	    nonlocal  while
+# and	 del       global 	not	      with
+# as	 elif	   if	    or	      yield
+# assert else	   import	pass	 
+# break	 except	   in	    raise
+
+# Identifiers
+#   --> Identifiers are the name used to identify a variable, function,
+#        class or an object
+#    --> Rules defined for naming an Identifiers:
+#            - No special character except underscore can be used as an identifier
+#            - Keyword should not be used as an idetifier name
+#            - Python is case insensitive, i.e Var and var are two different identifier
+#            - First character of an identifier can be character, underscore but not digit
